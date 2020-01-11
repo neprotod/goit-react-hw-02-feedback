0ac913b393eddf15dc8566341d823c752a38707b
Alexander Podlesnyi <gwebdzen@gmail.com>
2020-01-11 16:26:49 +0200

    Updates

---
+++ precache-manifest.33325c2719d683ea7828741709bd94c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32eac245d32d670c3478430a9086d0b8",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "7d72c62daf8a71a94411",
+    "url": "/goit-react-hw-02-feedback/static/css/main.2f3ea702.chunk.css"
+  },
+  {
+    "revision": "b68d148b37b5b8137921",
+    "url": "/goit-react-hw-02-feedback/static/js/2.4057e17c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-02-feedback/static/js/2.4057e17c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7d72c62daf8a71a94411",
+    "url": "/goit-react-hw-02-feedback/static/js/main.a5960c66.chunk.js"
+  },
+  {
+    "revision": "2df88248c4b7617979c9",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.6ab87472.js"
+  }
+]);
